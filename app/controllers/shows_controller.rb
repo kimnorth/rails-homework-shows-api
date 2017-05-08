da7e0ee21bcafc2918882c2814b66b923c5800d6
kimnorth <benignhumour@gmail.com>
2017-05-08 17:08:43 +0100

    Setup index route.

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,12 @@
+class ShowsController < ApplicationController
+
+  def shows_params
+    params.require(:show).permit([:title, :series, :description, :image, :programmeID])
+  end
+
+  def index
+    shows = Show.all
+    render :json => shows
+  end
+
+end
